f1c3760508f72cbf1c9b6a868770547e0b92934c
kiraGit67 <kirsten@sais.de>
2023-05-30 15:13:27 +0200

    adds src/components/BaseList.vue

---
+++ src/components/BaseList.vue
@@ -0,0 +1,22 @@
+<template>
+  <slot name="header">
+    <h2>{{ headline }}</h2>
+  </slot>
+  <div class="intro-text"><slot /></div>
+  <ul>
+    <li v-for="item in listItems" :key="item.id">{{ item.text }}</li>
+  </ul>
+</template>
+
+<script>
+const timestamp = +new Date();
+export default {
+  props: {
+    headline: String,
+    listItems: {
+      type: Array,
+      default: () => [{ text: "No items exist yet", id: timestamp }],
+    },
+  },
+};
+</script>
